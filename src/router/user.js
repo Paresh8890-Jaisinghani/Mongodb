9e490a70105b03139e6ebaf15ea017a2c907fa1b
Paresh8890-Jaisinghani <136781047+Paresh8890-Jaisinghani@users.noreply.github.com>
2024-11-23 23:43:10 +0530

    text

--- src/router/user.js
+++ src/router/user.js
@@ -41,39 +41,39 @@ router.get('/users/:id', async (req, res) => {
 })
 
 
-router.patch('/users/:id',async(req,res)=>{
+router.patch('/users/:id', async (req, res) => {
 
-    //taking the keys entered by the users in postman
+    //taking the keys entered by the users in postman (client side)
     const updates = Object.keys(req.body)
-    const allowedupdates = ['name','email','password','age']
-    const isvalidoperation = updates.every((update)=>allowedupdates.includes(update))
+    const allowedupdates = ['name', 'email', 'password', 'age']
+    const isvalidoperation = updates.every((update) => allowedupdates.includes(update))
 
-    if(!isvalidoperation){
-        return res.status(400).send({error : 'Invalid updates'})
+    if (!isvalidoperation) {
+        return res.status(400).send({ error: 'Invalid updates' })
     }
 
-    try{
-        const user =await User.findByIdAndUpdate(req.params.id,req.body,{new : true,runValidators : true});
-        if(!user){
+    try {
+        const user = await User.findByIdAndUpdate(req.params.id, req.body, { new: true, runValidators: true });
+        if (!user) {
             return res.status(404).send();
         }
         res.send(user)
     }
-    catch(e){
+    catch (e) {
         res.status(400).send(e);
     }
 })
 
 
-router.delete('/users/:id',async(req,res)=>{
+router.delete('/users/:id', async (req, res) => {
     const user = await User.findByIdAndDelete(req.params.id);
-    try{
-        if(!user){
+    try {
+        if (!user) {
             return res.status(400).send();
         }
         res.send(user)
     }
-    catch(e){
+    catch (e) {
         res.status(400).send(e);
     }
 })
